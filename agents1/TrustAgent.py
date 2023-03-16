0c17fe8aa3b55d2701e42382ad350890ff6b9166
Bart van Maris <bartvanmaris@hotmail.nl>
2023-03-16 17:30:42 +0100

    add rechecking of claimed rooms, no trust update yet

--- agents1/TrustAgent.py
+++ agents1/TrustAgent.py
@@ -330,6 +330,7 @@ class TrustAgent(BaselineAgent):
                                    and 'Door' in room['class_inheritance']
                                    and room['room_name'] not in self._searchedRooms
                                    and room['room_name'] not in self._tosearch]
+                unsearchedRooms.append(self._claimedSearchedRooms)
                 # If all areas have been searched but the task is not finished, start searching areas again
                 if self._remainingZones and len(unsearchedRooms) == 0:
                     self._tosearch = []
