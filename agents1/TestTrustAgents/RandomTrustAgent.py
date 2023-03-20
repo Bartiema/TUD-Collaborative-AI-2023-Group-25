018e7173792e70e0bc6f55a59a406d9a68148969
Nienke <N.C.A.Driessen@student.tudelft.nl>
2023-03-20 19:32:51 +0100

    Fixed random agent by changing the import

--- agents1/TestTrustAgents/RandomTrustAgent.py
+++ agents1/TestTrustAgents/RandomTrustAgent.py
@@ -1,4 +1,4 @@
-from random import random
+import random as rand
 
 from agents1.TrustAgent import TrustAgent
 from matrx.agents import StateTracker, Navigator
@@ -7,8 +7,8 @@ from matrx.agents import StateTracker, Navigator
 class RandomTrustAgent(TrustAgent):
     def get_trust(self, folder):
         trustBeliefs = {}
-        competence = random.randint(-10, 10)/10
-        willingness = random.randint(-10, 10) / 10
+        competence = rand.randint(-10, 10)/10
+        willingness = rand.randint(-10, 10) / 10
         trustBeliefs[self._humanName] = {'competence': competence, 'willingness': willingness}
         print("random competence = ", competence, " willingness = ", willingness)
         return trustBeliefs
